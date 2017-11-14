2a665ce3c9e734c3809803eb9dafa06aaeef42bc
apatron100 <30960286+apatron100@users.noreply.github.com>
2017-11-14 14:27:07 -0800

    setup flashcards

---
+++ BasicCard.js
@@ -0,0 +1,23 @@
+function BasicCard(question, answer) {
+	this.question = question;
+	this.answer = answer;
+}
+
+var questions = [
+	{
+		"question1":"Who was the first president of the USA?",
+		"answer": "George Washington"
+	},
+	{
+		"question2":"Who was the last president of the USA?",
+		"answer": "Barak Obama"
+	},
+	{
+		"question3":"Who is the actual president of the USA?",
+		"answer": "Donald Trump"
+	}
+
+]
+// Inquirer
+
+ 
